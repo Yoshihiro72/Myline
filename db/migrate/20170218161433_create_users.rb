e107170c1adb19c093753500700a83b8fd39723f
yoshihiro72 <yoshihiro.twenty@gmail.com>
2017-02-20 00:24:11 +0000

    create user model and registration page

--- db/migrate/20170218161433_create_users.rb
+++ db/migrate/20170218161433_create_users.rb
@@ -1,9 +1,12 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string :name
-      t.string :email
-      t.string :password_digest
+      t.string :name, :null => false
+      t.string :user_id, :null => false
+      t.string :email, :null => false
+      t.string :password_digest, :null => false
+      t.string :room_ids
+      t.string :friend_ids
 
       t.timestamps null: false
     end
