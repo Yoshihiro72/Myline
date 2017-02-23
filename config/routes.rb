62833b5eb4b4abf33db5d615c8ed666de2084de9
yoshihiro72 <yoshihiro.twenty@gmail.com>
2017-02-23 02:43:04 +0000

    add and remove friends by Ajax call

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   root 'home#index'
 
   get 'friends/' => 'friends#index'
-  post 'friends/add' => 'friends#add'
+  post 'friends/modify' => 'friends#modify'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
